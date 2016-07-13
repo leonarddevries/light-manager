a50ec7d8b861cca84a395df91793c894887e22a1
Leonard de Vries <leonard@ldvengineering.nl>
2016-07-13 21:28:11 +0200

    fix version number for test scripts

--- tests/context.py
+++ tests/context.py
@@ -3,6 +3,9 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 import lightmanager
-print("Testing lightmanager version {}".format(lightmanager.version))
+from lightmanager.version import __version__
+
+
+print("Testing lightmanager version {}. source located at: {}".format(__version__, lightmanager.__path__))
 
 # In every test file: from context import lightmanager
